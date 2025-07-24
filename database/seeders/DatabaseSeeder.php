c144ad14ab1ed515b065d38e11f1fb97b3053494
Hector Nevado <hectornevado46@gmail.com>
2025-07-24 18:39:01 +0200

    Creación seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Question;
+use App\Models\Answer;
+use App\Models\Comment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +17,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Hector Nevado',
+            'email' => 'hector@hnevado.es',
         ]);
+
+        $categories = Category::factory(10)->create();
+
+        $questions = Question::factory(30)->create([
+            'user_id' => fn() => User::inRandomOrder()->first()->id,
+            'category_id' => fn() => $categories->random()->id,
+        ]);
+
+        $answer = Answer::factory(50)->create([
+            'user_id' => fn() => User::inRandomOrder()->first()->id,
+            'question_id' => fn() => $questions->random()->id,
+        ]);
+
+        Comment::factory(100)->create([
+            'user_id' => fn() => User::inRandomOrder()->first()->id,
+            'commentable_id' => fn() => $answer->random()->id,
+            'commentable_type' => Answer::class,
+        ]);
+
+        Comment::factory(100)->create([
+            'user_id' => fn() => User::inRandomOrder()->first()->id,
+            'commentable_id' => fn() => $answer->random()->id,
+            'commentable_type' => Question::class,
+        ]);
+
     }
 }
