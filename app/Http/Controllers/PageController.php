544b604c5c070e42a32d52db212db99f0afd79d2
Hector Nevado <hectornevado46@gmail.com>
2025-07-24 19:28:35 +0200

    Creadas relaciones en Question, PageController, ruta y vista home

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Question;
+use App\Models\Category;
+use App\Models\User;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $questions = Question::with('category','user')->latest()->get();
+        
+        return view('pages.home', [
+            'questions' => $questions,
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,9 @@ use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [PageController::class,'index'])->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
